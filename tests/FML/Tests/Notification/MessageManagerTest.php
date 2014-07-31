edb17f85c82c2c82c9d79149f3673e43c2489c79
Patryk Szlagowski <patryksz@lsnova.pl>
2014-07-31 11:03:16 +0200

    new test for messagemanager

---
+++ tests/FML/Tests/Notification/MessageManagerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace FML\Tests\Notification;
+
+use FML\Notification\Example\Channel\OutputChannel;
+use FML\Notification\Example\Message\PlainMessage;
+use FML\Notification\MessageManager;
+use FML\Notification\Recipient\Recipient;
+
+class MessageManagerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testSendMessages()
+    {
+        $recipient = new Recipient();
+        $recipient
+            ->setAddress('1111')
+            ->setName('test');
+        $message = new PlainMessage();
+        $message
+            ->setSubject('test')
+            ->addRecipient($recipient)
+            ->setParameters(array(
+                    'foo' => 'bar'
+                ));
+        $outputChannel = new OutputChannel();
+
+        $manager = new MessageManager();
+        $manager
+            ->addChannel($outputChannel)
+            ->addMessage($message);
+
+        ob_start();
+        $manager->flush();
+        $content = trim(str_replace("\r\n", " ", ob_get_clean()));
+
+        $this->assertEquals($content, "test Hello test 1111  foo:bar");
+    }
+}
